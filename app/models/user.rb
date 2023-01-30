7cfa090f01f1b0354217a11b2d9690e0f705cfc4
Aqsa Altaf <aqsaaltaf01@gmail.com>
2023-01-30 09:58:45 +0000

    Device implemented with user STI

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# User
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  def admin?
+    type == 'Admin'
+  end
+
+  def super_admin?
+    type == 'SuperAdmin'
+  end
+
+  def client?
+    type == 'Client'
+  end
+end
