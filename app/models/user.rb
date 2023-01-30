1990f5ca9b1349aabb7c2fcd42512d212d434dd4
Aqsa Altaf <aqsaaltaf01@gmail.com>
2023-01-30 11:54:09 +0000

    Added Validatiob to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
+  validates :first_name, presence: true
+  validates :phone_number, presence: true, numericality: { only_integer: true }
+  validates :password, format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/,
+    message: 'Password must contain one upercase,one lowercase and one special character' } # rubocop :disable Layout/HashAlignment
+
   def admin?
     type == 'Admin'
   end
