ca342f0f756746cb117d0bb35c73dc60c715ed44
Aqsa Altaf <aqsaaltaf01@gmail.com>
2023-02-01 08:54:39 +0000

    admin/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.first_or_create(email: ' admin@projectname.com') do |user|
+  user.first_name = 'admin'
+  user.last_name = 'campencounter'
+  user.country = 'pakistan'
+  user.phone_number = '03194773106'
+  user.password = 'Password123!@#'
+end
