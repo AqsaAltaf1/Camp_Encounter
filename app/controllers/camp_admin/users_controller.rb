4ce0e08483081fe750820926137baac75d47bea8
Aqsa Altaf <aqsaaltaf01@gmail.com>
2023-02-05 04:36:00 +0000

    add pagination

--- app/controllers/camp_admin/users_controller.rb
+++ app/controllers/camp_admin/users_controller.rb
@@ -8,28 +8,12 @@ module CampAdmin
     def edit; end
 
     def index
-      @users = if params[:q].present?
-                 User.all.where('first_name like ?', params[:q])
-               else
-                 User.all
-               end
+      @users = User.all.page(params[:page])
+      if params[:q].present?
+        @users = User.all.where("first_name LIKE ? OR last_name LIKE ? OR email LIKE ? ",
+          "%#{params[:q]}%", "%#{params[:q]}%","%#{params[:q]}%")
+      end
     end
-
-    # def search
-    #    @users =User.where("first_name like ?", "%"+params[:q]+"%")
-    # end
-    # if params[:search_by_first_name] && params[:search_by_first_name] != ""
-    #   @users = @users.where("name like ?",
-    #   "%# {params[:search_by_first_name]}%")
-    # end
-    #   if params[:search_by_color] && params[:search_by_color] != ""
-    #     @kittens = @kittens.where("color like ?",
-    #     "%# {params[:search_by_color]}%")
-    #   end
-    #  if params[:search_by_age] && params[:search_by_age] != ""
-    #     @kittens = @kittens.where("age like ?",
-    #     "%# {params[:search_by_color]}%")
-    #   end
 
     def show; end
 
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,4 +3,5 @@
 # Record
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  paginates_per 3
 end
