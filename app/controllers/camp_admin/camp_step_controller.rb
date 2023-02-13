ed00559a27aad3f342973752d6c2579c9ec92808
Aqsa Altaf <aqsaaltaf01@gmail.com>
2023-02-13 11:02:25 +0000

    camp steps

--- app/controllers/camp_admin/camp_step_controller.rb
+++ app/controllers/camp_admin/camp_step_controller.rb
@@ -1,17 +1,11 @@
 module CampAdmin
   class CampStepController < ApplicationController
     include Wicked::Wizard
-    steps :personal_info, :second_step, :third_step, :forth_step
+    steps :personal_info, :second_step, :third_step, :forth_step, :fifth_step, :sixth_step, :seventh_step
 
     def show
       @user = current_user
       render_wizard
-    end
-
-    def update
-      @user = current_user
-      @user.attributes = params[:user]
-      render_wizard @user
     end
 
   end
