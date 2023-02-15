a94cd372370e0395ee261da12aeaf68c4f418592
Aqsa Altaf <aqsaaltaf01@gmail.com>
2023-02-15 15:35:38 +0000

    step added all

--- app/controllers/camp_admin/camp_step_controller.rb
+++ app/controllers/camp_admin/camp_step_controller.rb
@@ -1,6 +1,7 @@
 module CampAdmin
   class CampStepController < ApplicationController
     include Wicked::Wizard
+    before_action :set_progress
     steps *User.form_steps
 
     def show
@@ -20,12 +21,17 @@ module CampAdmin
 
     private
 
+    def set_progress
+      @total_steps = 5
+      @current_step = step || 1
+    end
+
     def redirect_to_finish_wizard
       redirect_to root_url, notice: "Thank you for signing up."
     end
 
     def user_params
-      params.require(:client).permit(:first_name, :last_name, :email, :country, :imag, :Date_of_birth,:Current_age, :Gender)
+      params.require(:client).permit(:first_name, :last_name, :email, :country, :image, :Date_of_birth,:Current_age, :Gender, :agree, :agreement, :reason, :option)
     end
   end
 end
