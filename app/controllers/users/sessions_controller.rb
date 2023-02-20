e5afaa5bb40086fe731dd080fb992c2f42e2cfcd
Aqsa Altaf <aqsaaltaf01@gmail.com>
2023-02-20 02:02:46 +0000

    added

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,7 @@ module Users
   # comment
   class SessionsController < Devise::SessionsController
     def after_sign_in_path_for(_resource)
-      if current_user&.client? && UserApplication.includes(:user_id)
-        root_path
-      elsif current_user.client?
+      if current_user.client?
         active_camp_camp_admin_location_path(current_user)
       else
         root_path
